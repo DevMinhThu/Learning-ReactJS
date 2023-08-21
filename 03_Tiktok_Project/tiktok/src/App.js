b1ec12d1f14a3eebd458cc38c2b1d5507265ed99
Vũ Minh Thư <devminhthu2023@gmail.com>
2023-08-21 22:14:10 +0700

    feat: useImperativeHandle

--- 03_Tiktok_Project/tiktok/src/App.js
+++ 03_Tiktok_Project/tiktok/src/App.js
@@ -7,10 +7,12 @@ import UseEffectHook from "./components/UseEffectHook.tsx";
 import PreviewAvatar from "./components/PreviewAvatar.tsx";
 import FakeChatApp from "./components/FakeChatApp.tsx";
 import UseLayoutEffect from "./components/UseLayoutEffect.tsx";
+import UseImperativeHandle from "./components/UseImperativeHandle.tsx";
 
 function App() {
   return (
     <div className="App">
+      <UseImperativeHandle />
       <UseLayoutEffect />
       <FakeChatApp />
       <LearnUseState />
